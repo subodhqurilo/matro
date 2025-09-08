0a4f870c0a8f2ff2a6d9e70e7c2c15c5af01e1c7
amancoderdev <mr.amankr99@gmail.com>
2025-09-08 11:39:33 +0530

    Update next.config.ts to ignore ESLint during build

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // <-- Add this line
+  },
   images: {
     domains: ["images.pexels.com", "res.cloudinary.com", "localhost"],
   },
 };
 
 export default nextConfig;
+//hgit
